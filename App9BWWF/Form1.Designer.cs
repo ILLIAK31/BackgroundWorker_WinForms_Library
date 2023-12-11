074c0246753e3eb4343a6bda7731d1880e1ed3ff
Illia <karma@MYPC>
2023-12-11 17:55:49 +0100

    Add project files.

---
+++ App9BWWF/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace App9BWWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            button1 = new System.Windows.Forms.Button();
+            progressBar1 = new System.Windows.Forms.ProgressBar();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(219, 66);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(298, 88);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new System.Drawing.Point(126, 265);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new System.Drawing.Size(522, 67);
+            progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(progressBar1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ App9BWWF/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassBackgroundWorker;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace App9BWWF
+{
+    public partial class Form1 : Form
+    {
+        private ClassBW backgroundWorker;
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker = new ClassBW();
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            backgroundWorker.Error += BackgroundWorker_Error;
+        }
+        private int[,] GenerateRandomMatrix(int rows, int cols)
+        {
+            Random rand = new Random();
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rand.Next(1, 10); 
+                }
+            }
+            return matrix;
+        }
+        private void MultiplyMatrices(object argument)
+        {
+            if (argument is not MatrixMultiplicationArgs args)
+                return;
+            int[,] matrixA = args.MatrixA;
+            int[,] matrixB = args.MatrixB;
+            int rowsA = matrixA.GetLength(0);
+            int colsA = matrixA.GetLength(1);
+            int colsB = matrixB.GetLength(1);
+            int[,] result = new int[rowsA, colsB];
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsB; j++)
+                {
+                    for (int k = 0; k < colsA; k++)
+                    {
+                        result[i, j] += matrixA[i, k] * matrixB[k, j];
+                    }
+                    int progress = (i * colsB + j + 1) * 100 / (rowsA * colsB);
+                    backgroundWorker.ReportProgress(progress);
+                }
+            }
+        }
+        private async void BackgroundWorker_ProgressChanged(object sender, ClassBackgroundWorker.ProgressChangedEventArgs e)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                progressBar1.Invoke(new Action(() => BackgroundWorker_ProgressChanged(sender, e)));
+            }
+            else
+            {
+                progressBar1.Value = e.ProgressPercentage;
+                if (e.ProgressPercentage == 100)
+                {
+                    await Task.Delay(500); 
+                    MessageBox.Show("Matrix multiplication completed!", "Task Complete");
+                }
+            }
+        }
+        private void BackgroundWorker_Error(object sender, ClassBackgroundWorker.ErrorEventArgs e)
+        {
+            MessageBox.Show($"Error occurred: {e.Exception.Message}");
+        }
+        private class MatrixMultiplicationArgs
+        {
+            public int[,] MatrixA { get; }
+            public int[,] MatrixB { get; }
+            public MatrixMultiplicationArgs(int[,] matrixA, int[,] matrixB)
+            {
+                MatrixA = matrixA;
+                MatrixB = matrixB;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = 1000; // Set the size for both matrices to be size x size
+            int[,] matrixA = GenerateRandomMatrix(size, size);
+            int[,] matrixB = GenerateRandomMatrix(size, size);
+            backgroundWorker.RunWorkerAsync(MultiplyMatrices, new MatrixMultiplicationArgs(matrixA, matrixB));
+        }
+    }
+}
